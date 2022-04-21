1f554ac003e0944f929708e23e6bea32832f7bad
woodytexas <squrooted@gmail.com>
2022-04-20 18:17:01 -0700

    putting on repo lmao

--- src/main/java/io/github/woodiertexas/architecture_extensions/ArchitectureExtensions.java
+++ src/main/java/io/github/woodiertexas/architecture_extensions/ArchitectureExtensions.java
@@ -1,15 +1,15 @@
-package com.example.example_mod;
+package io.github.woodiertexas.architecture_extensions;
 
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class ArchitectureExtensions implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod name as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("Example Mod");
+	public static final Logger LOGGER = LoggerFactory.getLogger("Architecture Extensions");
 
 	@Override
 	public void onInitialize(ModContainer mod) {
