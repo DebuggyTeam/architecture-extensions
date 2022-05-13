5ee016d42daf47e3032e58cf9d7d20690b97258b
woodiertexas <woodytexas8088@gmail.com>
2022-05-12 22:52:53 -0700

    [release] releasing v0.2.0-pre1 for blanketcon usage
    Signed-off-by: woodiertexas <woodytexas8088@gmail.com>

--- src/main/resources/data/ArchExModelProvider.java
+++ src/main/resources/data/ArchExModelProvider.java
@@ -1,3 +1,4 @@
+/*
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockStateDefinitionProvider;
 import net.minecraft.data.Main;
@@ -6,13 +7,13 @@ import net.minecraft.data.client.model.TextureKey;
 
 public class ArchExModelProvider extends FabricBlockStateDefinitionProvider {
 
-    /*
+
     The end goal is to have every JSON file in ../resources/.. automatically generated.
     That includes "../resources/assets/blockstates", "../resources/assets/models/..", and "../resources/data/architecture_extensions/recipes"
     Also the code should be well-documented so it can be updated as new blocktypes are added to arch-ex over time or referenced as needed for other project.
 
     Thanks, woodiertexas
-     */
+
 
     public static final Model ARCH_BLOCK_MODEL = ArchExModelProvider.createModel("arch_block", TextureKey.ALL);
 
@@ -30,3 +31,6 @@ public class ArchExModelProvider extends FabricBlockStateDefinitionProvider {
         modelGenerator.register(Main.thonkAgain, Models.GENERATED);
     }
 }
+*/
+
+
