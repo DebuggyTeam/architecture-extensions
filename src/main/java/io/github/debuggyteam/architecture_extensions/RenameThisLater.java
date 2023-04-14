f88a5dde17c807afde94af5d6fa0b5c4f1714b90
woodiertexas <woodietexas@protonmail.com>
2023-04-13 18:38:39 -0700

    Delete RenameThisLater.java

--- src/main/java/io/github/debuggyteam/architecture_extensions/RenameThisLater.java
+++
@@ -1,9 +0,0 @@
-package io.github.debuggyteam.architecture_extensions;
-
-import io.github.debuggyteam.architecture_extensions.api.BlockGroup;
-import io.github.debuggyteam.architecture_extensions.api.BlockType;
-import net.minecraft.block.Block;
-
-public record RenameThisLater(BlockType type, BlockGroup group, Block block) {
-
-}
