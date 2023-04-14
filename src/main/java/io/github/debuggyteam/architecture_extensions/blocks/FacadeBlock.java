c4ce6bdab7ff37eb75523202b59d20a17558e625
woodiertexas <woodietexas@protonmail.com>
2023-04-13 22:42:19 -0700

    recipe json file stuff

--- src/main/java/io/github/debuggyteam/architecture_extensions/blocks/FacadeBlock.java
+++ src/main/java/io/github/debuggyteam/architecture_extensions/blocks/FacadeBlock.java
@@ -1,21 +1,32 @@
 package io.github.debuggyteam.architecture_extensions.blocks;
 
 import net.minecraft.block.AbstractLichenBlock;
+import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.LichenSpreadBehavior;
 import net.minecraft.block.ShapeContext;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
+import net.minecraft.world.WorldAccess;
 
 /**
  * @author Gaming32 & Portal Cubed devs
  * @since arch-ex v2.1.0-1.19.4
  **/
 public class FacadeBlock extends AbstractLichenBlock {
+	public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
+
 	public FacadeBlock(Settings settings) {
 		super(settings);
+		this.setDefaultState(this.getDefaultState().with(WATERLOGGED, false)); // Thanks LambdAurora!
 	}
 
 	@Override
@@ -23,9 +34,27 @@ public class FacadeBlock extends AbstractLichenBlock {
 		return VoxelShapes.empty();
 	}
 
-
 	@Override
 	public LichenSpreadBehavior getLichenSpreadBehavior() {
 		return new LichenSpreadBehavior(this);
+	}
+
+	// Deals with block waterlogging. Thanks acikek!
+	@Override
+	public FluidState getFluidState(BlockState state) {
+		return state.get(WATERLOGGED) ? Fluids.WATER.getStill(false) : Fluids.EMPTY.getDefaultState();
+	}
+
+	@Override
+	public BlockState getStateForNeighborUpdate(BlockState state, Direction direction, BlockState neighborState, WorldAccess world, BlockPos pos, BlockPos neighborPos) {
+		if (state.get(WATERLOGGED)) world.scheduleFluidTick(pos, Fluids.WATER, Fluids.WATER.getTickRate(world));
+
+		return state;
+	}
+
+	@Override
+	protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+		super.appendProperties(builder);
+		builder.add(WATERLOGGED);
 	}
 }
