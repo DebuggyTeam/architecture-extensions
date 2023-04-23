71d40ca15dcdc59e29b1ce4fba65dd3c8bcbbdf8
Falkreon <falkreon@gmail.com>
2023-04-22 20:21:30 -0500

    Make the voxelshapes fancy

--- src/main/java/io/github/debuggyteam/architecture_extensions/blocks/PostLanternBlock.java
+++ src/main/java/io/github/debuggyteam/architecture_extensions/blocks/PostLanternBlock.java
@@ -4,26 +4,39 @@ import io.github.debuggyteam.architecture_extensions.util.VoxelHelper;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.ShapeContext;
-import net.minecraft.datafixer.fix.ChunkPalettedStorageFix.Facing;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.fluid.Fluids;
 import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.Properties;
+import net.minecraft.util.function.BooleanBiFunction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.util.shape.VoxelShapes;
 import net.minecraft.world.BlockView;
-import net.minecraft.world.World;
 import net.minecraft.world.WorldAccess;
 
 public class PostLanternBlock extends Block {
 	public static final BooleanProperty WATERLOGGED = Properties.WATERLOGGED;
 	public static final BooleanProperty HANGING = Properties.HANGING;
-
-	protected static final VoxelShape POST_CAP_SHAPE = Block.createCuboidShape(5.0, 0.0, 5.0, 11.0, 6.0, 11.0);
-	protected static final VoxelShape POST_CAP_HANGING_SHAPE = Block.createCuboidShape(5, 9, 5, 11, 15, 11);
+	
+	private static final VoxelShape LANTERN_BODY = Block.createCuboidShape(5, 0, 5, 11, 6, 11);
+	private static final VoxelShape LANTERN_CAP = Block.createCuboidShape(4, 5, 4, 12, 6, 12);
+	private static final VoxelShape LANTERN_FINIAL = Block.createCuboidShape(6, 6, 6, 10, 7, 10);
+	
+	private static final VoxelShape POST_LANTERN_SHAPE = VoxelShapes.combine(
+			VoxelShapes.combine(LANTERN_BODY, LANTERN_CAP, BooleanBiFunction.OR),
+			LANTERN_FINIAL, BooleanBiFunction.OR);
+	
+	private static final VoxelShape HANGING_LANTERN_BODY = Block.createCuboidShape(5, 9, 5, 11, 15, 11);
+	private static final VoxelShape HANGING_LANTERN_CAP = Block.createCuboidShape(4, 14, 4, 12, 15, 12);
+	private static final VoxelShape HANGING_LANTERN_FINIAL = Block.createCuboidShape(6, 15, 6, 10, 16, 10);
+	
+	private static final VoxelShape HANGING_POST_LANTERN_SHAPE = VoxelShapes.combine(
+			VoxelShapes.combine(HANGING_LANTERN_BODY, HANGING_LANTERN_CAP, BooleanBiFunction.OR),
+			HANGING_LANTERN_FINIAL, BooleanBiFunction.OR);
 
 	public PostLanternBlock(Settings settings) {
 		super(settings.luminance(state -> 15));
@@ -32,7 +45,7 @@ public class PostLanternBlock extends Block {
 
 	@Override
 	public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
-		return state.get(HANGING) ? POST_CAP_HANGING_SHAPE : POST_CAP_SHAPE;
+		return state.get(HANGING) ? HANGING_POST_LANTERN_SHAPE : POST_LANTERN_SHAPE;
 	}
 	
 	@Override
