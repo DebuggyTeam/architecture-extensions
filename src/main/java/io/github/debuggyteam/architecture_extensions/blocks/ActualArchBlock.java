6e55dd97369a079e3b039543bc316266f789e7c2
woodiertexas <woodietexas@protonmail.com>
2023-06-23 17:17:40 -0700

    some more stuff

--- src/main/java/io/github/debuggyteam/architecture_extensions/blocks/ActualArchBlock.java
+++ src/main/java/io/github/debuggyteam/architecture_extensions/blocks/ActualArchBlock.java
@@ -2,11 +2,45 @@ package io.github.debuggyteam.architecture_extensions.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.StairsBlock;
+import net.minecraft.block.HorizontalFacingBlock;
+import net.minecraft.block.enums.BlockHalf;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.DirectionProperty;
+import net.minecraft.state.property.EnumProperty;
+import net.minecraft.state.property.Properties;
 import org.quiltmc.qsl.block.extensions.api.QuiltBlockSettings;
 
-public class ActualArchBlock extends Block {
+public class ActualArchBlock extends HorizontalFacingBlock {
+	public static final DirectionProperty FACING = HorizontalFacingBlock.FACING;
+	public static final EnumProperty<BlockHalf> HALF = Properties.BLOCK_HALF;
+	
 	public ActualArchBlock(QuiltBlockSettings settings) {
 		super(settings);
+	}
+	
+	@Override
+	public BlockState getPlacementState(ItemPlacementContext context) {
+		// Thanks Falkreon
+		BlockHalf half = switch(context.getSide()) {
+			case UP -> BlockHalf.BOTTOM;
+			case DOWN -> BlockHalf.TOP;
+			default -> {
+				double sideHitHeight = context.getHitPos().getY() - context.getBlockPos().getY();
+				if (sideHitHeight > 0.5) {
+					yield BlockHalf.TOP;
+				} else {
+					yield BlockHalf.BOTTOM;
+				}
+			}
+		};
+		
+		return this.getDefaultState().with(FACING, context.getPlayerFacing()).with(HALF, half);
+	}
+	
+	@Override
+	protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+		super.appendProperties(builder);
+		builder.add(HALF);
 	}
 }
