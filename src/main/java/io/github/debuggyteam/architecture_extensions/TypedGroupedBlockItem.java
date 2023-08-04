2f088e39a845d7fe63c29cbc33fbcb131769e604
Falkreon <falkreon@gmail.com>
2023-08-04 01:27:32 -0500

    Null -> Missing

--- src/main/java/io/github/debuggyteam/architecture_extensions/TypedGroupedBlockItem.java
+++ src/main/java/io/github/debuggyteam/architecture_extensions/TypedGroupedBlockItem.java
@@ -47,7 +47,7 @@ public class TypedGroupedBlockItem extends BlockItem implements TypedGrouped {
 		
 		//This hover never actually displays since the tooltip is already a hover.
 		//But this keeps the Null tradition alive and prevents the tooltip from becoming gigantic.
-		return Text.literal("Null")
+		return Text.literal("Missing")
 				.styled(it -> it.withHoverEvent(
 						HoverEvent.Action.SHOW_TEXT.buildHoverEvent(Text.translatable(MISSING_LOCALIZATION_KEY, Text.literal(ideal)))
 						));
