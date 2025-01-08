c3b3baf47371e180fe2181f5057199764bc94aad
woodiertexas <woodietexas@protonmail.com>
2025-01-08 01:38:10 -0800

    tear everything out, repackage to `gay.debuggy` and prepare fabric port for mc1.20(.1)

--- src/main/java/io/github/debuggyteam/architecture_extensions/resource/json/BlockStateTemplate.java
+++
@@ -1,9 +0,0 @@
-package io.github.debuggyteam.architecture_extensions.resource.json;
-
-public final class BlockStateTemplate extends BaseTemplateJson<BlockStateTemplate> {
-
-	public BlockStateTemplate(String raw) {
-		super(raw);
-	}
-
-}
